b0c126ff6ecbe7e0aef8bd0a1d0065e9d19049ea
Usirusen Ikon <lexicon048@gmail.com>
2023-02-13 23:59:31 +0100

    Write test for Recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,11 +1,10 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_food
-  has_many :foods, through: :recipe_foods
 
-  validates :name, presence true
-  validates :preparation_time, presence true, numericality { only_integer: true, greater_than_or_equal_to: 1 }
-  validates :cooking_time, presence true, numericality { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :name, presence: true
+  validates :preparation_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :cooking_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 
   def recipe_list
     @recipe = Recipe.all.where(user_id: params[:id])
