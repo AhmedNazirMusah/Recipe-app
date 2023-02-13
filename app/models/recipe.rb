f1fdc204cad99f9c794e3ac78e4aaac6041e3d7d
Usirusen Ikon <lexicon048@gmail.com>
2023-02-13 19:09:52 +0100

    Write associations and validations for recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,13 @@
 class Recipe < ApplicationRecord
+  belongs_to :user
+  has_many :recipe_food
+  has_many :foods, through: :recipe_foods
+
+  validates :name, presence true
+  validates :preparation_time, presence true, numericality { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :cooking_time, presence true, numericality { only_integer: true, greater_than_or_equal_to: 1 }
+
+  def recipe_list
+    @recipe = Recipe.all.where(user_id: params[:id])
+  end
 end
