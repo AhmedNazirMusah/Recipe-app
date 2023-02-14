cc02debb2825ca44b65aaed5bfe20193392c7268
ahmednazirmusah <ahmed999nazir@gmail.com>
2023-02-14 14:48:04 +0000

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :recipes, dependent: :destroy
-         has_many :foods, dependent: :destroy
-       
-         validates :name, presence: true
-       
+  has_many :recipes, dependent: :destroy
+  has_many :foods, dependent: :destroy
 
+  validates :name, presence: true
 end
