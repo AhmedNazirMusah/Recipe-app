9600a0d7ab322dd4995b8a491a0b1843004d93ab
ahmednazirmusah <ahmed999nazir@gmail.com>
2023-02-23 13:14:30 +0000

    update

--- config/puma.rb
+++ config/puma.rb
@@ -29,6 +29,8 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
+
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
@@ -37,7 +39,8 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-# preload_app!
+preload_app!
+
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
